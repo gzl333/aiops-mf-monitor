50a239d9a76893ee0cacc2d18313026836150753
mimas <zlguo@cnic.cn>
2023-06-07 14:57:54 +0800

    feat: 增加关于页面模板

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,13 @@
 import { RouteRecordRaw } from 'vue-router'
 
+// @mimas: allocated root path of current app, defined in package.json
+const appPath = process.env.appPath as string
+
 const routes: RouteRecordRaw[] = [
   {
-    path: '/my/monitor',
+    path: appPath,
     component: () => import('layouts/MainLayout.vue'),
-    redirect: '/my/monitor/service1',
+    redirect: appPath + '/service1',
     children: [
       {
         path: 'service1',
